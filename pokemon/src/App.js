81a2f890a88293be1560f1353dc5db25456fb0d3
Lamia Rimty <rimtycse@gmail.com>
2021-11-17 19:05:41 +0600

    pokemon map

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React ,{useState,useEffect} from 'react';
+import PokemonList from './PokemonList';
+import axios from 'axios';
+
+ 
 
 function App() {
-  return (
-    
- null
-  );
+ const [pokemon, setPokemon] = useState()
+
+useEffect(()=>{
+  axios.get("https://pokeapi/api/v2/pokemon").then(res=>{
+    setPokemon(res.data.results.map(p=>p.name))
+  })
+},[])
+
+ return(
+    <PokemonList pokemon={pokemon}/>  //pass pokemon to pokemonList it can render our pokemon
+ );
 }
 
 export default App;
