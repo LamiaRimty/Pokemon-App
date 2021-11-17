90420643ecbe9a911b24a42749f30f3c317badf7
Lamia Rimty <rimtycse@gmail.com>
2021-11-17 21:07:29 +0600

    Laoding_State

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -10,11 +10,13 @@ function App() {
  const [currentPageUrl,setCurrentPageUrl]=useState(["https://pokeapi.co/api/v2/pokemon" ])
  const [nextPageUrl,setNextPageUrl] =useState()
  const [prevPageUrl,setPrevPageUrl]=useState()
+ const [loading,setLoading]=useState(true)
 
 useEffect(()=>{
   axios.get(currentPageUrl).then(res=>{
+    setLoading(false)
     setNextPageUrl(res.data.next)
-    setNextPageUrl(res.data.next)
+    setPrevPageUrl(res.data.next)
     setPokemon(res.data.results.map(p=>p.name))
   })
 },[currentPageUrl]) //every time reget pokemon,evry time go to different page
